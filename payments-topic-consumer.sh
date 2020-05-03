4302394517d7f44090924dd2b02f5b037bcade0c
hippalus <habiphakan.isler@gmail.com>
2020-05-03 21:16:09 +0300

    [docker-compose][Dockerfile] all services have been dockerized

--- payments-topic-consumer.sh
+++ payments-topic-consumer.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 docker run --net=host confluentinc/cp-enterprise-kafka:5.2.2  kafka-console-consumer --bootstrap-server localhost:9092  --topic payments-topic --from-beginning --group console-group
